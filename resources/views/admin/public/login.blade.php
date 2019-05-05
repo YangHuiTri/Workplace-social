7f309b54000fe2215ed3f6c7ffccadfc3fc602b0
YangHuiTri <913603802@qq.com>
2019-05-05 12:41:55 +0800

    更改后台登录背景
    更改后台登录背景

--- resources/views/admin/public/login.blade.php
+++ resources/views/admin/public/login.blade.php
@@ -24,20 +24,20 @@
 </head>
 <body>
 <input type="hidden" id="TenantId" name="TenantId" value="" />
-<div class="header"><h2 style="color: white;">&nbsp;&nbsp;&nbsp;职场社交-后台登录</h2></div>
+<div class="header" style="background: rgba(8,96,186, 0.1);"><h2 style="color: white;">&nbsp;&nbsp;&nbsp;职场社交-后台登录</h2></div>
 <div class="loginWraper">
   <div id="loginform" class="loginBox">
     <form class="form form-horizontal" action="/admin/public/check" method="post">
       <div class="row cl">
         <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
         <div class="formControls col-xs-8">
-          <input id="" name="username" type="text" placeholder="账户" class="input-text size-L">
+          <input name="username" type="text" placeholder="账户" class="input-text size-L">
         </div>
       </div>
       <div class="row cl">
         <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60e;</i></label>
         <div class="formControls col-xs-8">
-          <input id="" name="password" type="password" placeholder="密码" class="input-text size-L">
+          <input name="password" type="password" placeholder="密码" class="input-text size-L">
         </div>
       </div>
       <div class="row cl">
@@ -56,14 +56,14 @@
       {{ csrf_field() }}
       <div class="row cl">
         <div class="formControls col-xs-8 col-xs-offset-3">
-          <input name="" type="submit" class="btn btn-success radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;">
-          <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;">
+          <input name="" type="submit" class="btn btn-primary radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;" style="background: rgba(103,169,214, 0.2);">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;" style="background: rgba(106,169,214, 0.2); color: white;">
         </div>
       </div>
     </form>
   </div>
 </div>
-<div class="footer">Copyright © 2010 - 2018 Workplace. All Rights Reserved.</div>
+<div class="footer" style="background: rgba(8,96,186, 0.1);">Copyright © 2010 - 2019 Workplace. All Rights Reserved.</div>
 <script type="text/javascript" src="/admin/lib/jquery/1.9.1/jquery.min.js"></script> 
 <script type="text/javascript" src="/admin/static/h-ui/js/H-ui.min.js"></script>
 <script type="text/javascript" src="/admin/lib/layer/2.4/layer.js"></script>
